71958af633ecc3f1810989e7e3c19e7f4442c421
RoyTao <92853346@qq.com>
2020-06-30 17:32:00 +0800

    update .gitignore

---
+++ cloud-eureka-server7002/src/main/java/com/roytao/springcloud/EurekaApplication2.java
@@ -0,0 +1,17 @@
+package com.roytao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author RoyTao
+ * @since 2020/6/30 16:57
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication2 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication2.class,args);
+    }
+}
